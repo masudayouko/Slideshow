4cbc09fe35f3d0d249d2ccdb1cd8f1cdc856f6bc
masudayouko <youyo1204@gmail.com>
2020-06-26 14:24:02 +0000

    6-8

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(".theTarget").skippr({
+	tansition : 'fade',
+	speed : 1000,
+	easing : 'easeOutQuart',
+	navType : 'block',
+	childrenEleminType : 'div',
+	arrows : true,
+	autoplay : false,
+	autoplayDurantion : 5000,
+	keyboardOnAlways : true,
+	hidePrevious : false
+
+});
